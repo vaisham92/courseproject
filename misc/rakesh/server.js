aa41d7a4723a5d346c45c4ecc2a5c505a2c32a06
Rakesh Datta <rakesh.datta6@gmail.com>
2016-11-04 23:46:45 -0700

    Create server.js

---
+++ misc/rakesh/server.js
@@ -0,0 +1,33 @@
+var express = require('express')
+  , fs = require('fs')
+  , passport = require('passport');
+
+
+var env = process.env.NODE_ENV || 'development'
+  , config = require('./config/config')[env]
+  , auth = require('./config/middlewares/authorization')
+  , mongoose = require('mongoose');
+
+
+mongoose.connect(config.db);
+
+
+var models_path = __dirname + '/app/models'
+fs.readdirSync(models_path).forEach(function (file) {
+  require(models_path+'/'+file);
+});
+
+require('./config/passport')(passport, config);
+
+var app = express();
+
+require('./config/express')(app, config, passport);
+
+require('./config/routes')(app, passport, auth);
+
+
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log('app running on port '+port);
+
+exports = module.exports = app;
