e63567fd37bbcb1a48ed4207e7c644e1c3aff692
Rakesh Datta <rakesh.datta6@gmail.com>
2016-11-13 23:36:29 -0800

    auth 3

--- misc/rakesh/auth.js
+++ misc/rakesh/auth.js
@@ -1,14 +1,8 @@
 
-passport.use(new FacebookStrategy({
-    clientID: "1722289798092382",
-    clientSecret: "aec5235d637ee2d5f5a053434d925366",
-    callbackURL: "https://binarylearning.herokuapp.com"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-app.get('/', passport.authenticate('facebook'));
+module.exports	= {
+    'facebookAuth' : {
+    	'clientID' : '1722289798092382',
+    	'clientSecret' : 'aec5235d637ee2d5f5a053434d925366',
+    	'callbackURL' : 'https://binarylearning.herokuapp.com/auth/facebook/callback'
+    }
+}
