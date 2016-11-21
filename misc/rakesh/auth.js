d4b2edce41ed6bcbe2c5b52abef059993b0a85d8
Rakesh Datta <rakesh.datta6@gmail.com>
2016-11-20 19:41:01 -0800

    Update auth.js

--- misc/rakesh/auth.js
+++ misc/rakesh/auth.js
@@ -6,3 +6,14 @@ module.exports	= {
     	'callbackURL' : 'https://binarylearning.herokuapp.com/auth/facebook/callback'
     }
 }
+
+#TBR
+module.exports	= {
+    'facebooLoginAuth' : {
+    	'clientID' : '1722289798092382',
+    	'clientSecret' : 'aec5235d637ee2d5f5a053434d925366',
+    	'callbackURL' : 'https://binarylearning.herokuapp.com/auth/facebook/callback'
+    }
+}
+
+
