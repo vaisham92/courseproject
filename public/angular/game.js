7e3a03b8da5bc9db78788df5f20e8659fa210df5
Ashna Sebastian <Ashu@Ashnas-MacBook-Pro.local>
2016-11-30 16:33:43 -0800

    Adding new services

--- public/angular/game.js
+++ public/angular/game.js
@@ -57,7 +57,30 @@ binary.controller('gameController', function ($scope, $http, $routeParams, $loca
                         // checking the response data for statusCode
                         if (data.Status == 200) {
                              //window.location.assign("/levels");
-                             //console.log(data);
+                             console.log(data);
+                            
+                        } else if (data.Status == 401){
+                            $scope.inval_mess = data.Message;
+                        }
+                        else{
+                            $scope.inval_mess = "An unexpected error occured. Try again.";
+                        }
+
+                    }).error(function(error) {
+                            $scope.inval_mess = "An unexpected error occured. Try again.";
+                    });
+    }
+
+    function getRank(){
+
+                    $http({
+                        method: 'GET',
+                        url: '/api/getRank/' + $scope.session.testId + '/' + $scope.session.level
+                    }).success(function(data) {
+                        // checking the response data for statusCode
+                        if (data.Status == 200) {
+                             //window.location.assign("/levels");
+                             console.log(data);
                             
                         } else if (data.Status == 401){
                             $scope.inval_mess = data.Message;
